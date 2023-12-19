3f18b28af835fa04a920369b3e5ea0ad68c99cb6
Kristen Hammack <kristen.hammack@flightsafety.com>
2023-12-19 14:26:46 -0600

    Day1, Part 2

--- AoC2023/day1.cpp
+++ AoC2023/day1.cpp
@@ -3,19 +3,66 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
-static int get_line_number(const std::string& str)
+class number
 {
-	auto is_num = [](const char c) { return c - '0' >= 0 && c - '0' < 10; };
+public:
+	string name;
+	int num;
+	number(string name, const int num) :name(std::move(name)), num(num) {  }
+	
+};
 
-	const auto char1_ptr = std::find_if(begin(str), end(str), is_num);
-	if (char1_ptr == end(str)) return 0;
 
-	const auto char2_ptr = std::find_if(std::rbegin(str), std::rend(str), is_num);
-	if (char2_ptr == rend(str)) return 0;
-
-	return (*char1_ptr - '0') * 10 + *char2_ptr - '0';
+static int get_line_value(const string& str)
+{
+	auto lnum = number("", 0);
+	auto rnum = number("", 0);
+	const vector<number> numbers = {
+		number("one",1),
+		number("two",2),
+		number("three",3),
+		number("four",4),
+		number("five",5),
+		number("six",6),
+		number("seven",7),
+		number("eight",8),
+		number("nine",9)
+	};
+	auto min_index = str.length();
+	size_t max_index = 0;
+	for (const auto& num : numbers)
+	{
+		auto lindex = str.find(num.num + '0');
+		if (lindex != string::npos && lindex < min_index)
+		{
+			min_index = lindex;
+			lnum = num;
+		}
+		lindex = str.find(num.name);
+		if (lindex != string::npos && lindex < min_index)
+		{
+			min_index = lindex;
+			lnum = num;
+		}
+		auto rindex = str.rfind(num.num + '0');
+		if (rindex != string::npos && rindex > max_index)
+		{
+			max_index = rindex;
+			rnum = num;
+		}
+		rindex = str.rfind(num.name);
+		if (rindex != string::npos && rindex > max_index)
+		{
+			max_index = rindex;
+			rnum = num;
+		}
+	}
+	if (rnum.num == 0)rnum.num = lnum.num;
+	return lnum.num * 10 + rnum.num;
 }
 
 void day1::run()
@@ -27,7 +74,7 @@ void day1::run()
 		std::string line;
 		while (std::getline(file, line))
 		{
-			sum += get_line_number(line);
+			sum += get_line_value(line);
 		}
 	}
 	file.close();
