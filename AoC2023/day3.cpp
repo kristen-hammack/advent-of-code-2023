1635de948a6c9c71c59c21db37f2586888d292da
Kristen Hammack <kristen.hammack@flightsafety.com>
2023-12-27 11:49:38 -0600

    saving progress

--- AoC2023/day3.cpp
+++ AoC2023/day3.cpp
@@ -6,22 +6,98 @@
 #include <regex>
 #include <string>
 using namespace std;
-//adjacent if scan(row-1,row+1,begin(num)-1,end(num)+1) has anything not a digit and not a .
-bool scan(const string::const_iterator& first, const string::const_iterator& last)
+//count number of separate numbers found in the area
+int scan(const string::const_iterator& first, const string::const_iterator& last)
 {
-	const regex r("[^\\d\\.]");
-	return regex_search(first, last, r);
+	const regex r("\\d+");
+	auto result_begin = sregex_iterator(first, last, r);
+	auto result_end = sregex_iterator();
+	return distance(result_begin, result_end);
 }
 
 string::const_iterator get_next_digit(const string::const_iterator& first, const string::const_iterator& last)
 {
-	auto is_digit = [](const char c) {return c >= '0' && c <= '9'; };
-	return find_if(first, last, is_digit);
+	auto search = [](const char c) {return c >= '0' && c <= '9'; };
+	return find_if(first, last, search);
 }
 string::const_iterator get_next_non_digit(const string::const_iterator& first, const string::const_iterator& last)
 {
-	auto is_digit = [](const char c) {return c < '0' || c > '9'; };
-	return find_if(first, last, is_digit);
+	auto search = [](const char c) {return c < '0' || c > '9'; };
+	return find_if(first, last, search);
+}
+string::const_iterator get_next_star(const string::const_iterator& first, const string::const_iterator& last)
+{
+	auto search = [](const char c) {return c == '*'; };
+	return find_if(first, last, search);
+}
+
+tuple<int, string::const_iterator> get_int_next_to(const int index, const string& line)
+{
+	auto first = get_next_digit(begin(line), end(line));
+	string::const_iterator last;
+	auto found = false;
+	//while !found
+		//answer, start, length = get_next_int(begin(line)+start,end(line))
+		//if(answer<0) break;
+		//if(answer>0 && start >= index-1 && start + length <= index+1) ++found;
+		//start += length;
+	//return answer, begin(line)+start;
+	auto end_int = false;
+	while(first!=end(line) && !end_int)
+	{
+		int diff = distance(begin(line),end(line));
+		if (diff >= index - 1 && diff <= index + 1)found = true;
+
+	}
+}
+
+tuple<int,int,int> get_next_int(){}
+
+int get_power(int index, string before, string line, string after)
+{
+	auto is_digit = [](const char c) {return c >= '0' && c <= '9'; };
+	for (int i = 0; i < 2; ++i)
+	{
+		auto found = 0;
+		auto first_b = begin(before);
+		auto first_l = begin(line);
+		auto first_a = begin(after);
+		while(found<2)
+		{
+			//for each line
+			//get the pointer to the next digit
+			//until/unless we hit the end:
+				//if this digit touches the given gear, count it as found
+				//
+			first_b = find_if(first_b, end(before), is_digit);
+			while (first_b != end(before))
+			{
+				if (is_digit(*first_b))
+				{
+int diff = distance(begin(before), first_b);
+				if (diff >= index - 1 && diff < +index + 1)++found;
+					++first_b;
+				}
+				else { break; }
+				
+			}
+
+			first_l = find_if(first_l, end(line), is_digit);
+			while (first_l != end(line))
+			{
+				if (distance(first_l, first_l) == index)++found;
+				if (is_digit(*first_l))++first_l;
+			}
+
+			first_a = find_if(first_a, end(after), is_digit);
+			while (first_a != end(after))
+			{
+				if (distance(first_a, first_a) == index)++found;
+				if (is_digit(*first_a))++first_a;
+			}
+		}
+		
+	}
 }
 
 string get_padded_string(const string& str)
@@ -29,30 +105,38 @@ string get_padded_string(const string& str)
 	return "." + str + ".";
 }
 
-int sum_line(const string& before, const string& str, const string& after)
+bool is_gear(const string::const_iterator& before, const string::const_iterator& star,
+             const string::const_iterator& after)
 {
-	auto p_string = get_padded_string(str);
-	const auto p_before = get_padded_string(before);
-	const auto p_after = get_padded_string(after);
+	int i = 0;
+	i += scan(before - 1, before + 1);
+	i += scan(star - 1, star + 1);
+	i += scan(after - 1, after + 1);
+	return i==2;
+}
+
+int sum_line(const string& line_before, const string& str, const string& line_after)
+{
+	const auto p_string = get_padded_string(str);
+	const auto p_before = get_padded_string(line_before);
+	const auto p_after = get_padded_string(line_after);
 	const auto start_p_before = begin(p_before);
 	const auto start_p_after = begin(p_after);
 
 	int sum = 0;
-	auto first = get_next_digit(begin(p_string), end(p_string));
-	while (first != end(p_string))
+	auto star = get_next_star(begin(p_string), end(p_string));
+	while (star != end(p_string))
 	{
-		auto last = get_next_non_digit(first, end(p_string));
-		const auto offset = distance(cbegin(p_string), first);
-		const auto len = distance(first, last);
-		if (scan(start_p_before + offset-1, start_p_before + offset + len + 1)
-			|| scan(first - 1, last + 1)
-			|| scan(start_p_after + offset-1, start_p_after + offset + len + 1))
+		const auto offset = distance(cbegin(p_string), star);
+		auto before = start_p_before + offset;
+		auto after = start_p_after + offset;
+		if (is_gear(before, star, after))
 		{
-			sum += stoi(string(first, last));
+			sum += stoi(string(star, star));
 		}
-		first = get_next_digit(last, end(p_string));
+		star = get_next_star(star, end(p_string));
 	}
-	//cout << endl << before << endl << str << endl << after << endl << "Sum: " << sum << endl;
+	cout << endl << line_before << endl << str << endl << line_after << endl << "Sum: " << sum << endl;
 	return sum;
 }
 
--- AoC2023/day3.h
+++ AoC2023/day3.h
@@ -7,8 +7,8 @@ public:
 	static void run();
 };
 
-bool scan(const string::const_iterator& first, const string::const_iterator& last);
+int scan(const string::const_iterator& first, const string::const_iterator& last);
 string::const_iterator get_next_digit(const string::const_iterator& first, const string::const_iterator& last);
 string::const_iterator get_next_non_digit(const string::const_iterator& first, const string::const_iterator& last);
 string get_padded_string(const string& str);
-int sum_line(const string& before, const string& str, const string& after);
+int sum_line(const string& line_before, const string& str, const string& line_after);
