a1c4e65ea0f864908e04cd26e7e41fc80469da2d
Kristen Hammack <kristen.hammack@flightsafety.com>
2023-12-27 18:20:17 -0600

    putting it all together

--- AoC2023/day3.cpp
+++ AoC2023/day3.cpp
@@ -91,60 +91,15 @@ bool is_gear(const string::const_iterator& before, const string::const_iterator&
 	return i==2;
 }
 
-int sum_line(const string& line_before, const string& str, const string& line_after)
+int get_gear_power(const string& before, const string& star_line, const string& after, const int star_index)
 {
-	const auto p_string = get_padded_string(str);
-	const auto p_before = get_padded_string(line_before);
-	const auto p_after = get_padded_string(line_after);
-	const auto start_p_before = begin(p_before);
-	const auto start_p_after = begin(p_after);
-
-	int sum = 0;
-	auto star = get_next_star(begin(p_string), end(p_string));
-	while (star != end(p_string))
-	{
-		const auto offset = distance(cbegin(p_string), star);
-		auto before = start_p_before + offset;
-		auto after = start_p_after + offset;
-		if (is_gear(before, star, after))
-		{
-			sum += stoi(string(star, star));
-		}
-		star = get_next_star(star, end(p_string));
-	}
-	cout << endl << line_before << endl << str << endl << line_after << endl << "Sum: " << sum << endl;
-	return sum;
-}
-
-int do_work(ifstream& file)
-{
-	int sum = 0;
-	string line1;
-	string line2;
-	string line3;
-	if (!getline(file, line1)) { return sum; }
-	sum = sum_line(line1, line1, line1);
-
-	if (!getline(file, line2)) { return sum; }
-	sum += sum_line(line1, line1, line2);
-
-	while (true)
-	{
-		if (!getline(file, line3)) { return sum + sum_line(line1, line2, line2); }
-		sum += sum_line(line1, line2, line3);
-		line1 = line2;
-		line2 = line3;
-	}
-}
-
-int get_gear_power(const string& before, const string& star_line, const string& after,	const size_t star_index, string::const_iterator& first)
-{
+	//todo incorrectly identifying int at index+2 as part number
 	int gear_power = 0;
 	int result;
 	int nums_found = 0;
 	for (auto line : { before,star_line,after })
 	{
-		first = begin(line);
+		auto first = cbegin(line);
 		auto last = end(line);
 		while (first != last)
 		{
@@ -169,77 +124,96 @@ int get_gear_power(const string& before, const string& star_line, const string& 
 	return gear_power;
 }
 
+int sum_line(const string& line_before, const string& str, const string& line_after)
+{
+	const auto p_string = get_padded_string(str);
+	const auto p_before = get_padded_string(line_before);
+	const auto p_after = get_padded_string(line_after);
+	const auto start_p_before = begin(p_before);
+	const auto start_p_after = begin(p_after);
+
+	int sum = 0;
+	auto star = get_next_star(begin(p_string), end(p_string));
+	while (star != end(p_string))
+	{
+		const auto offset = distance(cbegin(p_string), star);
+		auto before = start_p_before + offset;
+		auto after = start_p_after + offset;
+		if (is_gear(before, star, after))
+		{
+			cout << endl << line_before << endl << str << endl << line_after << endl << "Gear Power (Gear at " << offset - 1 << "): ";
+			const int gear_power = get_gear_power(p_before, p_string, p_after, offset);
+			cout << gear_power << endl;
+			sum += gear_power;
+
+		}
+		star = get_next_star(star, end(p_string));
+	}
+	//cout << endl << line_before << endl << str << endl << line_after << endl << "Sum: " << sum << endl;
+	return sum;
+}
+
+int do_work(ifstream& file)
+{
+	int sum = 0;
+	string line1;
+	string line2;
+	string line3;
+	if (!getline(file, line1)) { return sum; }
+	sum = sum_line(line1, line1, line1);
+
+	if (!getline(file, line2)) { return sum; }
+	sum += sum_line(line1, line1, line2);
+
+	while (true)
+	{
+		if (!getline(file, line3)) { return sum + sum_line(line1, line2, line2); }
+		sum += sum_line(line1, line2, line3);
+		line1 = line2;
+		line2 = line3;
+	}
+}
+
 void day3::run()
 {
 	int sum = 0;
 
-	const string line1 = ".1....58.."; //0
-	const string line2 = ".....+755."; //0
-	const string line3 = "...$.*...."; //755*598, is_gear=true;
-	const string line4 = ".664.59..."; //
-	auto star = get_next_star(begin(line3), end(line3));
-	auto star_index = distance(begin(line3), star);
-	auto b = is_gear(begin(line2) + star_index, star, begin(line4) + star_index);
-	cout << "Star 1 Expected: true; actual: " << (b ? "true" : "false") << endl;
-	
-	int result, start, length;
-	tie(result, start, length) = get_next_int(begin(line4), end(line4));
-	cout << "Line 4 first int Expected: 664, 1, 3; actual: " << result << ", " << start << ", " << length << endl;
+	//const string line1 = ".1....58.."; //0
+	//const string line2 = ".....+755."; //0
+	//const string line3 = "...$.*...."; //755*598, is_gear=true;
+	//const string line4 = ".664.59..."; //
+	//auto star = get_next_star(begin(line3), end(line3));
+	//int star_index = distance(begin(line3), star);
+	//auto b = is_gear(begin(line2) + star_index, star, begin(line4) + star_index);
+	//cout << "Star 1 Expected: true; actual: " << (b ? "true" : "false") << endl;
+	//
+	//int result, start, length;
+	//tie(result, start, length) = get_next_int(begin(line4), end(line4));
+	//cout << "Line 4 first int Expected: 664, 1, 3; actual: " << result << ", " << start << ", " << length << endl;
 
-	tie(result, start, length) = get_next_int(begin(line4) + start + length, end(line4));
-	cout << "Line 4 second int Expected: 59, 5, 2; actual: " << result << ", " << start << ", " << length << endl;
+	//tie(result, start, length) = get_next_int(begin(line4) + start + length, end(line4));
+	//cout << "Line 4 second int Expected: 59, 5, 2; actual: " << result << ", " << start << ", " << length << endl;
 
-	auto first = begin(line1);
-	while(first!=end(line1))
-	{
-		tie(result, start, length) = get_next_int(first, end(line1));
-		if (result >= 0) cout << "Found " << result << " at index " << start << " with length " << length << endl;
-		else break;
-		first += start + length;
-		sum += result;
-	}
-
-	const int gear_power = get_gear_power(line2, line3, line4, star_index, first);
-	cout << "Gear Power: " << gear_power << endl;
-
-	//b = scan(begin(line2), end(line2));
-	//cout << "Line 2 Expected: false; actual: " << (b ? "true" : "false") << endl;
-	//b = scan(begin(line3), end(line3));
-	//cout << "Line 3 Expected: true; actual: " << (b ? "true" : "false") << endl;
-	//b = scan(begin(line4), end(line4));
-	//cout << "Line 4 Expected: true; actual: " << (b ? "true" : "false") << endl;
-
-	//auto digit_begin = get_next_digit(begin(line2), end(line2));
-	//auto digit_end = get_next_non_digit(digit_begin, end(line2));
-	//string s(digit_begin, digit_end);
-	//cout << "Line 2 number Expected: 234; actual: " << s << endl;
-
-	//digit_begin = get_next_digit(begin(line4), end(line4));
-	//digit_end = get_next_non_digit(digit_begin, end(line4));
-	//s = string(digit_begin, digit_end);
-	//cout << "Line 4 number Expected: 1; actual: " << s << endl;
-
-	//sum += sum_line(line1, line1, line1);
-	//cout << line1 << endl << line1 << endl << line1 << endl << "Sum: " << sum << endl << endl;
-
-	//sum += sum_line(line1, line1, line2);
-	//cout << line1 << endl << line1 << endl << line2 << endl << "Sum: " << sum << endl << endl;
-
-	//sum += sum_line(line1, line2, line3);
-	//cout << line1 << endl << line2 << endl << line3 << endl << "Sum: " << sum << endl << endl;
-
-	//sum += sum_line(line2, line3, line4);
-	//cout << line2 << endl << line3 << endl << line4 << endl << "Sum: " << sum << endl << endl;
-
-	//sum += sum_line(line3, line4, line4);
-	//cout << line3 << endl << line4 << endl << line4 << endl << "Sum: " << sum << endl << endl;
-
-	//ifstream file("day3_input.txt");
-	//if (file.is_open())
+	//auto first = begin(line1);
+	//while(first!=end(line1))
 	//{
-	//	sum = do_work(file);
+	//	tie(result, start, length) = get_next_int(first, end(line1));
+	//	if (result >= 0) cout << "Found " << result << " at index " << start << " with length " << length << endl;
+	//	else break;
+	//	first += start + length;
+	//	sum += result;
 	//}
-	//file.close();
+
+	//const int gear_power = get_gear_power(line2, line3, line4, star_index);
+	//cout << "Gear Power: " << gear_power << endl;
+
+
+	ifstream file("day3_input.txt");
+	if (file.is_open())
+	{
+		sum = do_work(file);
+	}
+	file.close();
 	
 	cout << sum;
 }
